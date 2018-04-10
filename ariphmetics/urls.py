3dd14e46a183df0803e03030ac01defa52a9ce5a
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2018-04-10 19:31:27 +0300

    Started working on new template

--- ariphmetics/urls.py
+++ ariphmetics/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_count>', views.index, name='index'),
     path('solve/', views.solve, name='solve'),
+    path('template/user_list.html', ''),
 ]
