8f0fcb21ac77aa86c68644f697917f0ad043e5d0
Wera Barinova <alisawera@gmail.com>
2018-04-10 15:49:35 +0300

    Login page added. Redirecting to the application ariphmetics

--- mathtest/urls.py
+++ mathtest/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import urls
 from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('ariphmetics/', include('ariphmetics.urls')),
 ]
