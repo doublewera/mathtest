87fb028bffec27f26a7af48c0453fcb7551049f4
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2018-04-21 16:00:51 +0300

    Merging multiple questions branch to master

--- ariphmetics/urls.py
+++ ariphmetics/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'ariphmetics'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('user_list', views.user_list),
     path('<int:question_count>', views.index, name='index'),
+    path('answer_list', views.answer_list),
+    path('user_list', views.user_list),
     path('solve/', views.solve, name='solve'),
 ]
